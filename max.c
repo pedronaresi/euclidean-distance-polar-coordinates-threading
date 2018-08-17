ea42ffd3ec7e91134aba958fd52e7e4d2d794e9b
aluno <pmnaresi@hotmail.com>
2018-08-17 15:35:55 -0300

    quase

---
+++ max.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <math.h>
+
+#define N 1000
+
+/*
+#if DEZ_CINCO
+  #define N 100000
+#else
+  #define N 10000000
+#endif
+*/
+
+#define MAX_THREADS 4 //8
+
+
+double *r1, *r2, *teta1, *teta2;
+
+double *retorno;
+
+void *distancia_radial(void *tid) {
+  long thid;
+  long i;
+
+  thid = (long) tid;
+
+  for(i=thid; i<N; i+=MAX_THREADS){
+      //if (array[i]>maxloc) maxloc = array[i];
+      retorno[thid] = sqrt((pow(r1[i],2) + pow(r2[i],2) - 2*r1[i]*r2[i]*cos(teta1[i]-teta2[i])));
+  }
+
+  printf("thread=%ld, result=%f\n", thid, retorno[thid]);
+
+  pthread_exit(0);
+} 
+
+int main(void){
+  pthread_t t[MAX_THREADS];
+  double maxglobal;
+  long i;
+  long th;
+
+  struct timeval inicio, final2;
+  int tmili;
+
+  r1 = (double*) malloc(N*sizeof(double));
+  r2 = (double*) malloc(N*sizeof(double));
+  teta1 = (double*) malloc(N*sizeof(double));
+  teta2 = (double*) malloc(N*sizeof(double));
+
+  retorno = (double*) malloc(MAX_THREADS*sizeof(double));
+
+  for(i=0; i<N; i++) 
+      r1[i] = (double) (rand()%100);
+      r2[i] = (double) (rand()%100);
+
+  for(i=0; i<N; i++) 
+      teta1[i] = (double) (rand()%360) ;
+      teta2[i] = (double) (rand()%360);
+
+  gettimeofday(&inicio, NULL);
+
+  for(th=0; th<MAX_THREADS; th++) {
+    pthread_create(&t[th], NULL, distancia_radial, (void *) th);
+  }
+
+  maxglobal = r1[0];
+  for(th=0; th<MAX_THREADS; th++) {
+    pthread_join(t[th],NULL);
+    if (retorno[th]>maxglobal) maxglobal = retorno[th];
+  }
+
+  gettimeofday(&final2, NULL);
+  tmili = (int) (1000 * (final2.tv_sec - inicio.tv_sec) + (final2.tv_usec - inicio.tv_usec) / 1000);
+
+  printf("\ntempo decorrido: %d milisegundos\n", tmili);
+
+  printf("maior valor=%f\n",maxglobal);
+
+  return 0;
+}  
