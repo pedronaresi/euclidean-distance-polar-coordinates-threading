60a5f2aabacc403cc76cb49990f75d0ac0ffb874
gaborin <gabriel_borin.lock@hotmail.com>
2018-08-23 13:50:48 -0300

    C pronto

--- max.c
+++ max.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 #include <math.h>
 
-#define N 1000
+#define N 100000
 
 /*
 #if DEZ_CINCO
@@ -15,12 +15,12 @@
 #endif
 */
 
-#define MAX_THREADS 4 //8
+#define MAX_THREADS 1 //8
 
 
 double *r1, *r2, *teta1, *teta2;
 
-double *retorno;
+double *result;
 
 void *distancia_radial(void *tid) {
   long thid;
@@ -30,10 +30,10 @@ void *distancia_radial(void *tid) {
 
   for(i=thid; i<N; i+=MAX_THREADS){
       //if (array[i]>maxloc) maxloc = array[i];
-      retorno[thid] = sqrt((pow(r1[i],2) + pow(r2[i],2) - 2*r1[i]*r2[i]*cos(teta1[i]-teta2[i])));
+      result[thid] = sqrt((pow(r1[i],2) + pow(r2[i],2) - 2*r1[i]*r2[i]*cos(teta1[i]-teta2[i])));
   }
 
-  printf("thread=%ld, result=%f\n", thid, retorno[thid]);
+  printf("Thread %ld finalizou.\n", thid);
 
   pthread_exit(0);
 } 
@@ -52,15 +52,17 @@ int main(void){
   teta1 = (double*) malloc(N*sizeof(double));
   teta2 = (double*) malloc(N*sizeof(double));
 
-  retorno = (double*) malloc(MAX_THREADS*sizeof(double));
+  result = (double*) malloc(MAX_THREADS*sizeof(double));
 
-  for(i=0; i<N; i++) 
+  for(i=0; i<N; i++){
       r1[i] = (double) (rand()%100);
       r2[i] = (double) (rand()%100);
+  }
 
-  for(i=0; i<N; i++) 
-      teta1[i] = (double) (rand()%360) ;
+  for(i=0; i<N; i++){
+      teta1[i] = (double) (rand()%360);
       teta2[i] = (double) (rand()%360);
+  }
 
   gettimeofday(&inicio, NULL);
 
@@ -68,18 +70,18 @@ int main(void){
     pthread_create(&t[th], NULL, distancia_radial, (void *) th);
   }
 
-  maxglobal = r1[0];
+  //maxglobal = r1[0];
   for(th=0; th<MAX_THREADS; th++) {
     pthread_join(t[th],NULL);
-    if (retorno[th]>maxglobal) maxglobal = retorno[th];
+    //if (result[th]>maxglobal) maxglobal = result[th];
   }
 
   gettimeofday(&final2, NULL);
   tmili = (int) (1000 * (final2.tv_sec - inicio.tv_sec) + (final2.tv_usec - inicio.tv_usec) / 1000);
 
-  printf("\ntempo decorrido: %d milisegundos\n", tmili);
+  printf("\ntempo decorrido para o calculo: %d milisegundos\n", tmili);
 
-  printf("maior valor=%f\n",maxglobal);
+  //printf("maior valor=%f\n",maxglobal);
 
   return 0;
-}  
+}
